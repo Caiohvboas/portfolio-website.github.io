2eab409426baa2c8fc00bcd0c5cc550ae38a0a05
Caiohvboas <116687690+Caiohvboas@users.noreply.github.com>
2025-04-09 02:35:10 -0300

    Building the Navbar & Setting Up the Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const hero = () => {
+  return <div className="w-full h-screen">
+    <Navbar />
+  </div>;
+};
+
+export default hero;

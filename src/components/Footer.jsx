a2356b1f4fee7507b313f42cfafee33731b4be5b
Caiohvboas <116687690+Caiohvboas@users.noreply.github.com>
2025-05-12 23:54:58 -0300

    Correção da palavra contact no componente footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="bg-gray-900 dark:bg-gray-200 text-white dark:text-gray-900 py-6 mt-12">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
         <div className="mb-4 md:mb-0">
-          <p className="text-lg font-semibold">Contato:</p>
+          <p className="text-lg font-semibold">Contact:</p>
           <p>E-mail: <a href="https://mail.google.com/mail/u/0/?pli=1#inbox?compose=new" target="_blank" rel="noopener noreferrer" className="underline hover:text-yellow-500">caio.boas@hotmail.com</a></p>
         </div>
         <div className="flex space-x-6 mb-4 md:mb-0">
